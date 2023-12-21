cfb7467271894a8a15682cf0b9bd4f2fd9ffe6ce
atharva07 <atharva.hiwase07@gmail.com>
2023-12-22 03:17:05 +0530

    SQL Configuration added for database testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,25 @@
 const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const sqlServer = require('cypress-sql-server');
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  config.db = {
+    userName: "ath",
+    password: "Martin#123",
+    server: "athcypressdb.database.windows.net",
+    options: {
+        database: "atharvahiwasedb",
+        encrypt: true,
+        rowCollectionOnRequestCompletion : true
+    }
+  }
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
-
   on("file:preprocessor", browserify.default(config));
+
+  tasks = sqlServer.loadDBPlugin(config.db);
+  on('task', tasks);
 
   // Make sure to return the config object as it might have been modified by the plugin.
   return config;
---
+++ cypress/integration/examples/sqldb.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+context('Window', () => {
+    it('Database Interaction', () => 
+    {
+        cy.sqlServer("select * from Persons").then(function(result)
+        {
+            console.log(result)
+        })
+    })
+})
