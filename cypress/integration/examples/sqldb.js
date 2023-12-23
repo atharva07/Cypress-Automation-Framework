e348e6e46a615b0c7ac2856d2c1381bf46adac71
atharva07 <atharva.hiwase07@gmail.com>
2023-12-24 00:11:42 +0530

    SQL integration integration

--- cypress/integration/examples/sqldb.js
+++ cypress/integration/examples/sqldb.js
@@ -6,18 +6,20 @@ context('Window', () => {
     {
         cy.sqlServer("select * from Persons").then(function(result)
         {
-            data = result
+            //data = result
+            console.log(result[1][3])
         })
     })
 
-    it('cy.wait() - wait for a specific amount of time', () => 
-    {
-        cy.get('.wait-input1').type(data[0][2])
-        cy.wait(1000)
-        cy.get('.wait-input2').type(data[1][2])
-        cy.wait(1000)
-        cy.wait('.wait-input3').type('wait 1000ms after typing')
-        cy.wait(1000)
+    //it('cy.wait() - wait for a specific amount of time', () => 
+    //{
+        //console.log(result[1][3])
+        // cy.get('.wait-input1').type(data[0][2])
+        // cy.wait(1000)
+        // cy.get('.wait-input2').type(data[1][2])
+        // cy.wait(1000)
+        // cy.wait('.wait-input3').type('wait 1000ms after typing')
+        // cy.wait(1000)
 
-    })
+    //})
 })
