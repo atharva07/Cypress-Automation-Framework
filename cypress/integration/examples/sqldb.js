bb12de3aec0337c39a3b79bffd6b456380b9a8c0
atharva07 <atharva.hiwase07@gmail.com>
2023-12-24 00:03:29 +0530

    SQL integrated execution

--- cypress/integration/examples/sqldb.js
+++ cypress/integration/examples/sqldb.js
@@ -1,11 +1,23 @@
 /// <reference types="Cypress" />
 
 context('Window', () => {
+    let data
     it('Database Interaction', () => 
     {
         cy.sqlServer("select * from Persons").then(function(result)
         {
-            console.log(result)
+            data = result
         })
+    })
+
+    it('cy.wait() - wait for a specific amount of time', () => 
+    {
+        cy.get('.wait-input1').type(data[0][2])
+        cy.wait(1000)
+        cy.get('.wait-input2').type(data[1][2])
+        cy.wait(1000)
+        cy.wait('.wait-input3').type('wait 1000ms after typing')
+        cy.wait(1000)
+
     })
 })
